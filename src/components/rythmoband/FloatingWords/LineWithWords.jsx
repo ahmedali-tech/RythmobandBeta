78ae33dd939c9fa1a8b4743f9a6434d1cda7bcf6
VladislavPastushenko <vlad1023333@gmail.com>
2022-10-21 01:10:06 +0200

    Fixed dealogue width for short phrases

--- src/components/rythmoband/FloatingWords/LineWithWords.jsx
+++ src/components/rythmoband/FloatingWords/LineWithWords.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "./LineWithWords.module.css";
 
-const DELAY = -0.2; // Delay of subtitles
+const DELAY = -0.4; // Delay of subtitles
 const PIXELS_WIDTH = 200; // Width multiplier
 const FONT_SIZE = 19
 
@@ -48,11 +48,15 @@ export default function LineWithWords({
       {dialogue.map((el, idx) => {
         let margin = idx % 2 === 0;
         const elContainerWidth = (time[idx][1] - time[idx][0]) * PIXELS_WIDTH
-        const elTextWidth = el.length * 11.5 // 11.5 is average width of letter of font-size 19
+        const elTextWidth = el.length * 9.5 // 11.5 is average width of letter of font-size 19
 
         let fontSize = FONT_SIZE
+        let letterSpacing = 'inherit'
         if (elContainerWidth < elTextWidth) {
-            fontSize = FONT_SIZE - 3
+            let diff = elTextWidth - elContainerWidth
+            diff = diff > 120 ? 120 : diff;
+            fontSize = FONT_SIZE - Math.round(diff / 20)
+            letterSpacing = '-1px'
         }
         return (
           <div
@@ -61,6 +65,7 @@ export default function LineWithWords({
               width: ( (time[idx][1] - time[idx][0]) / ((duration - DELAY) / 100)) + '%',
               marginTop: margin && "1em",
               fontSize: fontSize + 'px',
+              letterSpacing: letterSpacing
             }}
             className={styles.dialogue}
             key={time[idx]}
